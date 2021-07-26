318337ad5984728ebbf69170ce6286e38ebff068
Anshu Sharma <mycodebook15@gmail.com>
2021-07-26 19:36:10 +0530

    new 26 update design structure

---
+++ ds26new/snippet.js
@@ -0,0 +1,319 @@
+// {
+// 	// Place your snippets for html here. Each snippet is defined under a snippet name and has a prefix, body and
+// 	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+// 	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the
+// 	// same ids are connected.
+// 	// Example:
+// 	// "Print to console": {
+// 	// 	"prefix": "log",
+// 	// 	"body": [
+// 	// 		"console.log('$1');",
+// 	// 		"$2"
+// 	// 	],
+// 	// 	"description": "Log output to console"
+// 	// }
+// 	"h1 Jumbo Heading": {
+// 		"prefix": "h1jumbo",
+// 		"body": [
+// 			"<!-- Jumbo Wordblock heading h1 Jumbo-->",
+// 			"<div class=\"wordblock p4\">",
+// 			"    <div class=\"wordblock__hero\">",
+// 			"        <p class=\"mb-3\"><span class=\"wordblock__tag mtag diml\"> Web and App development company $1</span></p>",
+// 			"        <h1 class=\"wordblock__heading\">  Tecnologies that we use to build Stuning websites $2 </h1>",
+// 			"        <p class=\"wordblock__para\">We appreciate your trust greatly. Our clients choose us",
+// 			"            and our products because they know we are the best. $3</p>",
+// 			"        <div class=\"p2\">",
+// 			"            <button class=\"wordblock__btn mbtn\">Call Us $4</button>",
+// 			"        </div>",
+// 			"    </div>",
+// 			"</div>",
+// 			"<!-- Wordblock end -->"
+// 		],
+// 		"description": "h1 Jumbo Heading"
+// 	},
+// 	"h1 wordblock": {
+// 		"prefix": "h1wb",
+// 		"body": [
+// 			"<!-- H1  Wordblock heading-->",
+// 			"<div class=\"wordblock p4\">",
+// 			"    <div class=\"wordblock__hero\">",
+// 			"        <p class=\"mb-3\"><span class=\"wordblock__tag mtag diml\"> Web and App development company $1</span></p>",
+// 			"        <h1>  Tecnologies that we use to build Stuning websites $2 </h1>",
+// 			"        <p class=\"wordblock__para\"> We appreciate your trust greatly. Our clients choose us",
+// 			"            and our products because they know we are the best. $3</p>",
+// 			"        <div class=\"p2\">",
+// 			"            <button class=\"wordblock__btn mbtn\">Call Us $4</button>",
+// 			"        </div>",
+// 			"    </div>",
+// 			"</div>",
+// 			"<!-- Wordblock end -->"
+// 		],
+// 		"description": "h1 wordblock"
+// 	},
+// 	"h2 wordblock": {
+// 		"prefix": "h2wb",
+// 		"body": [
+// 			"<!-- H2 Wordblock heading-->",
+// 			"<div class=\"wordblock p4\">",
+// 			"    <div class=\"wordblock__hero\">",
+// 			"        <p class=\"mb-3\"><span class=\"wordblock__tag mtag diml\">Web and App development company $1</span></p>",
+// 			"        <h2>Tecnologies that we use to build Stuning websites $2</h1>",
+// 			"            <p class=\"wordblock__para\">We appreciate your trust greatly. Our clients choose us",
+// 			"                and our products because they know we are the best. $3</p>",
+// 			"            <div class=\"p2\">",
+// 			"                <button class=\"wordblock__btn mbtn\">Call Us $4</button>",
+// 			"            </div>",
+// 			"    </div>",
+// 			"</div>",
+// 			"<!-- Wordblock end -->"
+// 		],
+// 		"description": "h2 wordblock"
+// 	},
+// 	"Center Heading wordblock": {
+// 		"prefix": "cenwb",
+// 		"body": [
+// 			"  <!-- center Section -->",
+// 			"    <section class=\"section w-100\">",
+// 			"        <div class=\"container\">",
+// 			"            <div class=\"row jcc aic\">",
+// 			"                <div class=\"col-md-8\">",
+// 			"                    <div class=\"wordblock p4\">",
+// 			"                        <div class=\"wordblock__hero txtc\">",
+// 			"                            <p class=\"mb-3\"><span class=\"wordblock__tag mtag diml\">Web and App development company $1</span></p>",
+// 			"                            <h2>Tecnologies that we use to build Stuning websites $2</h1>",
+// 			"                                <p class=\"wordblock__para\">We appreciate your trust greatly. Our clients choose us",
+// 			"                                    and our products because they know we are the best. $3</p>",
+// 			"                                <div class=\"p2\">",
+// 			"                                    <button class=\"wordblock__btn mbtn\">Call Us $4</button>",
+// 			"                                </div>",
+// 			"                        </div>",
+// 			"                    </div>",
+// 			"                </div>",
+// 			"            </div>",
+// 			"        </div>",
+// 			"    </section>",
+// 			"    <!-- Center Section End -->"
+// 		],
+// 		"description": "Center Heading wordblock"
+// 	},
+// 	"section of 2": {
+// 		"prefix": "sec2",
+// 		"body": [
+// 			"  <!-- Section of 2 -->",
+// 			"    <section class=\"section bod w-100\">",
+// 			"        <div class=\"container bod\">",
+// 			"            <div class=\"row\">",
+// 			"                <div class=\"col-md-6 bod\">",
+// 			"",
+// 			"                    $1",
+// 			"",
+// 			"                </div>",
+// 			"                <div class=\"col-md-6 bod\">",
+// 			"",
+// 			"                    $2",
+// 			"",
+// 			"                </div>",
+// 			"            </div>",
+// 			"        </div>",
+// 			"    </section>",
+// 			"    <!-- End of Section 2 -->"
+// 		],
+// 		"description": "section of 2"
+// 	},
+// 	"section of 3": {
+// 		"prefix": "sec3",
+// 		"body": [
+// 			"    <!-- Section of 3 -->",
+// 			"    <section class=\"section bod w-100\">",
+// 			"        <div class=\"container bod\">",
+// 			"            <div class=\"row\">",
+// 			"                <div class=\"col-md-4 bod\">",
+// 			"                    $1",
+// 			"                </div>",
+// 			"                <div class=\"col-md-4 bod\">",
+// 			"                    $2",
+// 			"                </div>",
+// 			"                <div class=\"col-md-4 bod\">",
+// 			"                    $3",
+// 			"                </div>",
+// 			"            </div>",
+// 			"        </div>",
+// 			"    </section>",
+// 			"    <!-- End of Section 3 -->"
+// 		],
+// 		"description": "section of 3"
+// 	},
+// 	"section of 4": {
+// 		"prefix": "sec4",
+// 		"body": [
+// 			" <!-- Section of 4 -->",
+// 			"    <section class=\"section bod w-100\">",
+// 			"        <div class=\"container bod\">",
+// 			"            <div class=\"row\">",
+// 			"                <div class=\"col-md-3 bod\">",
+// 			"                    $1",
+// 			"                </div>",
+// 			"                <div class=\"col-md-3 bod\">",
+// 			"                    $2",
+// 			"                </div>",
+// 			"                <div class=\"col-md-3 bod\">",
+// 			"                    $3",
+// 			"                </div>",
+// 			"                <div class=\"col-md-3 bod\">",
+// 			"                    $4",
+// 			"                </div>",
+// 			"            </div>",
+// 			"        </div>",
+// 			"    </section>",
+// 			"    <!-- End of Section 2 -->"
+// 		],
+// 		"description": "section of 4"
+// 	},
+// 	"button": {
+// 		"prefix": "btnr5",
+// 		"body": [
+// 			"<button class=\"bdr_5\">Learn More</button>"
+// 		],
+// 		"description": "button"
+// 	},
+// 	"small btn": {
+// 		"prefix": "sbtn",
+// 		"body": [
+// 			"<button class=\"sbtn\">$1</button>"
+// 		],
+// 		"description": "small btn"
+// 	},
+// 	"medium btn": {
+// 		"prefix": "mbtn",
+// 		"body": [
+// 			"<button class=\"mbtn\">$1</button>"
+// 		],
+// 		"description": "medium btn"
+// 	},
+// 	"large btn": {
+// 		"prefix": "lbtn",
+// 		"body": [
+// 			"<button class=\"lbtn\">Click me</button>"
+// 		],
+// 		"description": "large btn"
+// 	},
+// 	"small tag": {
+// 		"prefix": "stag",
+// 		"body": [
+// 			" <span class=\"stag\">$1</span>"
+// 		],
+// 		"description": "small tag"
+// 	},
+// 	"medium tag": {
+// 		"prefix": "mtag",
+// 		"body": [
+// 			"<span class=\"mtag\">$1</span>"
+// 		],
+// 		"description": "medium tag"
+// 	},
+// 	"large tag": {
+// 		"prefix": "ltag",
+// 		"body": [
+// 			"<span class=\"ltag\">$1</span>"
+// 		],
+// 		"description": "large tag"
+// 	},
+// 	"paragraph": {
+// 		"prefix": "pc",
+// 		"body": [
+// 			"<p>looking Scarlett Johansson once. As a matter of fact, this got us thinking. About burgers and the",
+// 			"                    ways people love eating them. Found in every part of the world and a favorite among various age",
+// 			"                    groups from freckled teenagers to the wiser generation, $1</p>"
+// 		],
+// 		"description": "paragraph"
+// 	},
+// 	"featured card": {
+// 		"prefix": "fcard",
+// 		"body": [
+// 			"    <!-- 3 Container Section -->",
+// 			"    <div class=\"container\">",
+// 			"        <div class=\"row justify-content-center bod\">",
+// 			"            <div class=\"col-md-10 bod\">",
+// 			"                <div class=\"row justify-content-center\">",
+// 			"                    <!-- F_card -->",
+// 			"                    <div class=\"col-md-4 px-4 py-2 mb-4 bod\">",
+// 			"                        <a href=\"\" class=\"nodecoration\">",
+// 			"                            <div class=\"fcard\">",
+// 			"                                <img src=\"/img/bg2.jpg\" alt=\"\" class=\"fcard_img\">",
+// 			"                                <div class=\"fcard_content\">",
+// 			"                                    <h3 class=\"head_h3 mb-1\">Veg Combo</h3>",
+// 			"                                    <p class=\"fcard_p\">looking Scarlett Johansson once. As a matter </p>",
+// 			"                                    <button class=\"fcard_btn\"> Order Now</button>",
+// 			"                                </div>",
+// 			"                            </div>",
+// 			"                        </a>",
+// 			"                    </div>",
+// 			"                    <!-- end Fcard -->",
+// 			"                    <!-- F_card -->",
+// 			"                    <div class=\"col-md-4 px-4 py-2 mb-4 bod\">",
+// 			"                        <a href=\"\" class=\"nodecoration\">",
+// 			"                            <div class=\"fcard\">",
+// 			"                                <img src=\"/img/bg3.jpg\" alt=\"\" class=\"fcard_img\">",
+// 			"                                <div class=\"fcard_content\">",
+// 			"                                    <h3 class=\"head_h3 mb-1\">Biryani Burgur</h3>",
+// 			"                                    <p class=\"fcard_p\">looking Scarlett Johansson once. As a matter </p>",
+// 			"                                    <button class=\"fcard_btn\"> Order Now</button>",
+// 			"                                </div>",
+// 			"                            </div>",
+// 			"                        </a>",
+// 			"                    </div>",
+// 			"                    <!-- end Fcard -->",
+// 			"                    <!-- F_card -->",
+// 			"                    <div class=\"col-md-4 px-4 py-2 mb-4 bod\">",
+// 			"                        <a href=\"\" class=\"nodecoration\">",
+// 			"                            <div class=\"fcard\">",
+// 			"                                <img src=\"/img/bg4.jpg\" alt=\"\" class=\"fcard_img\">",
+// 			"                                <div class=\"fcard_content\">",
+// 			"                                    <h3 class=\"head_h3 mb-1\">Burger Pick</h3>",
+// 			"                                    <p class=\"fcard_p\">looking Scarlett Johansson once. As a matter </p>",
+// 			"                                    <button class=\"fcard_btn\"> Order Now</button>",
+// 			"                                </div>",
+// 			"                            </div>",
+// 			"                        </a>",
+// 			"                    </div>",
+// 			"                    <!-- end Fcard -->",
+// 			"",
+// 			"                </div>",
+// 			"            </div>",
+// 			"        </div>",
+// 			"    </div>",
+// 			"    <!-- End 3 container Section -->"
+// 		],
+// 		"description": "featured card"
+// 	},
+// 	"inputform": {
+// 		"prefix": "inform",
+// 		"body": [
+// 			"<div class=\"mb-4\">",
+// 			"    <div>",
+// 			"        <label for=\"$2\" class=\"inlable\">$2</label>",
+// 			"    </div>",
+// 			"    <div>",
+// 			"        <input type=\"$1\" class=\"instyle\" placeholder=\"$2\" name=\"$2\">",
+// 			"    </div>",
+// 			"    <div><span></span></div>",
+// 			"</div>"
+// 		],
+// 		"description": "input form"
+// 	},
+// 	"submit button": {
+// 		"prefix": "btnsubmit",
+// 		"body": [
+// 			"<button class=\"btn del_btn\">Submit</button>"
+// 		],
+// 		"description": "submit button"
+// 	},
+// 	// php Codes From Here
+// 	"php echo": {
+// 		"prefix": "pheco",
+// 		"body": [
+// 			"<?php echo $1 ?>"
+// 		],
+// 		"description": "php echo"
+// 	}
+// }
